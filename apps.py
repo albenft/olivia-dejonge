b82deee7f77ac183ca13b0f72574983019044892
alben <albenft@gmail.com>
2019-10-09 20:27:51 +0700

    create main class

--- apps.py
+++ apps.py
@@ -14,3 +14,12 @@ class MyStreamListener(tweepy.StreamListener):
         
     def on_error(self, status_code):
         print(status_code)
+
+if __name__ == "__main__":
+
+    litener = MyStreamListener()
+
+    auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
+    auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
+    
+    api = tweepy.API(auth)
