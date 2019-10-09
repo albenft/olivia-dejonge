a049f4244498ab0eb310cec1e6f5c53458641412
alben <albenft@gmail.com>
2019-10-10 01:09:33 +0700

    first runable application

--- apps.py
+++ apps.py
@@ -7,22 +7,53 @@ import numpy as np
 import credentials
 
 # my object models
-import entry_model
+from entry_model import MyCsvEntryModel
 
-class MyStreamListener(tweepy.StreamListener):
+class TweetStreamer():
+    """
+    Class for stream live tweets.
+    """
+    def stream_tweets(self, filename, tag_list, limit_entry):
+        listener = SaveFileListener(filename, limit_entry)
+        auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
+        auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
+        api = tweepy.API(auth)
+
+        stream = tweepy.Stream(auth = api.auth, listener = listener)
+        stream.filter(track=tag_list)
+
+class SaveFileListener(tweepy.StreamListener):
+    """
+    Basic streamer class which saves the results on an external.
+    """
+    def __init__(self, filename, limit_entry):
+        super(SaveFileListener, self).__init__()
+        self.filename = filename
+        self.entry_count = 0
+        self.limit_entry = limit_entry
 
     def on_status(self, status):
-        # TODO: process the statusses
-        return True
+        entry = MyCsvEntryModel(status)
+        if self.entry_count <= self.limit_entry:
+            print('entry count: ' + str(self.entry_count))
+            try:
+                with open(self.filename, 'a') as io:
+                    io.write(entry.to_entry() + '\n')
+                self.entry_count = self.entry_count + 1
+            except BaseException as e:
+                print('Error on_status: %s' % str(e))
+            return True
+        else:
+            return False
         
     def on_error(self, status_code):
         print(status_code)
 
 if __name__ == "__main__":
 
-    litener = MyStreamListener()
+    tag_list = ['jokowi']
+    filename = 'target/Jokowi_Tweets.csv'
+    limit_entry = 10
 
-    auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
-    auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
-    
-    api = tweepy.API(auth)
+    tweet_streamer = TweetStreamer()
+    tweet_streamer.stream_tweets(filename, tag_list, limit_entry)
--- entry_model.py
+++ entry_model.py
@@ -2,18 +2,13 @@ import tweepy
 import pandas as pd
 from datetime import datetime, timedelta, date
 
-class MyEntryModel(status):
+class MyCsvEntryModel(tweepy.Status):
     """
     A Class Represents CSV Entry For Inserting to an External File
     """
     def __init__(self, status):
         user = status._json['user']
         location = user['location']
-        location_city, location_province = '',''
-        if location != '':
-            location_split = location.split(', ')
-            location_city = location_split[0]
-            location_province = location_split[1]
         string_time = status._json['created_at']
         date = datetime.strptime(string_time, '%a %b %d %H:%M:%S %z %Y')
 
@@ -32,15 +27,14 @@ class MyEntryModel(status):
         self.date = datetime.strftime(date, '%Y-%m-%d %H:%M:%S')
         self.name = user['name']
         self.verified = 1 if user['verified'] == True else 0
-        self.location_city = location_city
-        self.location_province = location_province
-        self.text = text.replace(';','')
+        self.location = location if location != None else ''
+        self.text = text.replace(';','').replace('\n',' ').replace('  ',' ')
     
     def to_entry(self):
         """
         Build a semicolon separated value string to be inserted to a csv file
         """
-        return self.date + ';' + self.name + ';' + str(self.verified) + ';' + self.location_city + ';' + self.location_province + ';' + self.text
+        return self.date + ';' + self.name + ';' + str(self.verified) + ';' + self.location + ';' + self.text
 
         
 
